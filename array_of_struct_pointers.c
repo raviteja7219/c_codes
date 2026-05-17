14ca79133973a71197b28fe02a999c2a99b1058f
raviteja7219 <108860238+raviteja7219@users.noreply.github.com>
2026-05-17 18:52:38 +0530

    Add 'name' field to struct and update initialization

--- array_of_struct_pointers.c
+++ array_of_struct_pointers.c
@@ -7,6 +7,7 @@ struct node
 {
     int roll;
     float percent;
+    char name[20];
 };
 
 int main() 
@@ -19,17 +20,23 @@ int main()
     }
     s[0].roll = 2;
     s[0].percent = 91.23;
+    strcpy(s[0].name, "Raviteja");
     ptr[1]->roll = 3;
     ptr[1]->percent = 92.23;
+    strcpy(ptr[1]->name, "Ravish");
     ptr[2]->roll = 4;
     ptr[2]->percent = 93.23;
+    strcpy(ptr[2]->name, "Tejesh");
 
     printf("%d\n",(ptr[0]->roll));
     printf("%f\n",(ptr[0]->percent));
+    printf("%s\n",(ptr[0]->name));
     printf("%d\n",(ptr[1]->roll));
     printf("%f\n",(ptr[1]->percent));
+    printf("%s\n",(ptr[1]->name));
     printf("%d\n",(ptr[2]->roll));
     printf("%f\n",(ptr[2]->percent));
+    printf("%s\n",(ptr[2]->name));
     
     return 0;
 }
