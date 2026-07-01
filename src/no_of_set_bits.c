b0bb88794be46c4f20dc2efe4d3b5e9f6686d12e
rponukup <ravishastry24@gmail.com>
2026-07-01 14:30:54 +0530

    Adding the correct bitwise.c and no_of_set_bits.c files

---
+++ src/no_of_set_bits.c
@@ -0,0 +1,18 @@
+// program to calculate number of set bits in a number
+
+#include <stdio.h>
+
+int main()
+{
+    int num = 11;
+    int count = 0;
+
+    while (num)
+    {
+        num = num & (num - 1);
+        count++;
+    }
+    printf("No. of set bits = %d\n", count);
+
+    return 0;
+}
